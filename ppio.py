7ee237e3498975959bf78d57fd3ba275d81f9c6a
user-n <37430634+user-n@users.noreply.github.com>
2020-10-28 02:57:36 +0600

    Update ppio.py

--- ppio.py
+++ ppio.py
@@ -6,9 +6,9 @@ xcenter=math.ceil(scrWidth/2)-50
 ycenter=math.ceil(scrHeight/2)-50
 time.sleep(5)
 x,y=700,700
-pyautogui.moveTo(x, y)
-for i in range(x,801)
-	pyautogui.click(x,y)
+    pyautogui.moveTo(x, y)
+for i in range(x,801):
+pyautogui.click(x,y)
 # for first time only 1 line)
 pyautogui.alert('done')
 time.sleep(5)

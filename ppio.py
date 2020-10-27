19cf19165068d00736a5fbeee6601fc813f0476f
user-n <37430634+user-n@users.noreply.github.com>
2020-10-28 02:39:27 +0600

    ppio.py
    contact me if have ideas

---
+++ ppio.py
@@ -0,0 +1,14 @@
+import pyautogui
+import time
+import math
+scrWidth, scrHeight=pyautogui.size()
+xcenter=math.ceil(scrWidth/2)-50
+ycenter=math.ceil(scrHeight/2)-50
+time.sleep(5)
+x,y=700,700
+pyautogui.moveTo(x, y)
+for i in range(x,801)
+	pyautogui.click(x,y)
+# for first time only 1 line)
+pyautogui.alert('done')
+time.sleep(5)
